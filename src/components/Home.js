6324cc029260fbabf61e078118c37ff78ad76f55
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-01 22:31:22 +0300

    info about app

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,8 +10,20 @@ const Home = () => {
                         <div className="about-row">
                             <div className="about-content-wrapper">
                                 <h1 className="about-heading">
-                                    <span className="show-about-heading">Manage projects</span> &nbsp; work better
+                                    <span className="show-about-heading">Manage projects</span> &nbsp; work smarter
                                 </h1>
+                                <div className="main-content">
+                                    <p className="p-content">
+                                        We help manage your projects and enhance collaboration amongst your team. Join us today and watch your business grow
+                                    </p>
+                                    <div className="button-container">
+                                        <span className="sign-up-button">
+
+                                        </span>
+
+                                    </div>
+
+                                </div>
                             </div>
 
                         </div>
