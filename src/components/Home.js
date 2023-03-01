34712d7fc00b854d22a155df209714d438f845dd
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-01 21:36:15 +0300

    setting up navbar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,9 @@
 import "../styles/Home.css";
+import Navbar from "./Navbar";
 const Home = () => {
     return (  
         <div>
-          <div className="header-navigation">
-            <nav className="navigation-display">
-                <div className="showcase">
-                    <a href="/" className="logo-show">
-                        <div className="logo-wrapper">
-                            <h2><i className="fas fa-project-diagram" id="project-logo"></i><span>Project Management App</span></h2>
-                        </div>
-                    </a>
-                    <ul className="items-nav">
-                        <li> <a href="#" className="lists">About</a></li>
-                        <li> <a href="#" className="lists">Contact</a></li>
-                    </ul>
-                    <ul className="items-nav right-side">
-                        <li>
-                            <a href="/login" className="logo-show login1">
-                                <span>Log In</span>
-                            </a>
-                        </li>
-                        <li>
-                            <div className="get-started">
-                                <a href="/signup" className="signup">
-                                    <span className="gs-show">
-                                        <span className="gs-show2">Get Started</span>
-                                    </span>   
-                                </a>
-
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-           
-          </div>
+            <Navbar />
           <section className="about">
                 <div className="about-content">
                     <div className="about-column">
