bc2c63576bdb4c19df76726d6399857b81130d14
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-02 11:41:23 +0300

    styling sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,6 +11,50 @@ const Signup = () => {
                 </h1>
                 <h2 className="h2-signup">Sign Up With Project Management App </h2>
                 <div className="form-div">
+                    <form action="" className="sign-up-form">
+                        <div className="signup-entries">
+                            <div>
+                                <div>
+                                    <div className="email-entry">
+                                        <label htmlFor="" className="email-label"> Enter email to get started</label>
+                                    </div>
+                                    <div className="placeholder-email">
+                                        <input type="text" name="email" placeholder="email address" autoComplete="email" className="input-email1"/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="username-entry">
+                                        <label htmlFor="" className="username-label"> Enter Username</label>
+                                    </div>
+                                    <div className="placeholder-username">
+                                        <input type="text" name="username" placeholder="username"  className="input-username"/>
+                                    </div>
+                                </div>
+                                <div>
+
+                                    <div className="password-entry">
+                                        <label htmlFor="" className="password-label"> Enter Password</label>
+                                    </div>
+                                    <div className="placeholder-password">
+                                        <input type="password" name="password" placeholder="password" className="input-password"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" className="submit-signup">
+                                <div className="get-started-submission">Get Started</div>
+
+                            </button>
+                        </div>
+
+                    </form>
+
+                    <div className="already-signed-up">
+                        <p>
+                            Already have an account?
+                            <a></a>
+                        </p>
+                        
+                    </div>
 
                 </div>
 
