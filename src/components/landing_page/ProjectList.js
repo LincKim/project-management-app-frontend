293fae1c12923c3ba794f08276d6bb3981d64b75
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2023-03-06 00:30:34 +0300

    highlighting active project

--- src/components/landing_page/ProjectList.js
+++ src/components/landing_page/ProjectList.js
@@ -2,20 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 const ProjectList = ({projects, title}) => {
     return ( 
-        <div>
-            <h2>{title}</h2>
-            {
-                projects.map(project => (
-                    <div key={project.id}>
-                        <Link to={`/projects/${project.id}`}>
-                            <h2>{project.title}</h2>
-                            <p>{project.description}</p>
-                        
-                        </Link>
+        <div className="project-list">
+            <div>
+                
+                <div>
+
+                    <div className="wrapper1">
+
+                        <div className="project-detail-23">
+
+                            
+
+                                <h2 className="my-project-displayed">{title}</h2>
+                                {
+                                    projects.map(project => (
+                                        <div key={project.id} className="project-preview">
+                                            <Link to={`/projects/${project.id}`}>
+                                                <h2 className="my-project-displayed">{project.title}</h2>
+                                                <p className="description-project-now">{project.description}</p>
+                                            
+                                            </Link>
+
+                                        </div>
+                                    ))
+                                }
+
+                            
+                        </div>
 
                     </div>
-                ))
-            }
+
+                </div>
+
+            </div>
         </div>
      );
 }
